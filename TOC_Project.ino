cd196110ac140df1cacbd2832a93d3aa884dd248
Tenyson05 <Rojahog@hotmail.com>
2019-03-14 16:11:14 -0700

    Stoplight initiated

---
+++ TOC_Project.ino
@@ -0,0 +1,18 @@
+const int rpin = 3;
+const int ampin = 4;
+const int gpin = 5;
+const int wbutton = 6;
+//may have to change pin nums later
+void setup() {
+  // put your setup code here, to run once:
+  pinMode(rpin, HIGH);
+  pinMode(ampin, HIGH);
+  pinMode(gpin, HIGH); 
+   
+
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+
+}
