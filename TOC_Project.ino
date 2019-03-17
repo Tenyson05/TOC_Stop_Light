04dc3db328d685ab33eb99f6903aaededb317109
Tenyson05 <Rojahog@hotmail.com>
2019-03-17 12:14:32 -0700

    Added light functionality

--- TOC_Project.ino
+++ TOC_Project.ino
@@ -5,14 +5,29 @@ const int wbutton = 6;
 //may have to change pin nums later
 void setup() {
   // put your setup code here, to run once:
-  pinMode(rpin, HIGH);
-  pinMode(ampin, HIGH);
-  pinMode(gpin, HIGH); 
+  pinMode(rpin, OUTPUT);
+  pinMode(ampin, OUTPUT);
+  pinMode(gpin, OUTPUT); 
    
 
 }
 
 void loop() {
   // put your main code here, to run repeatedly:
+  digitalWrite(gpin, HIGH);
+  digitalWrite(ampin, LOW);
+  digitalWrite(rpin, LOW);
+  delay(1000);
+  digitalWrite(gpin, LOW);
+  digitalWrite(ampin, HIGH);
+  digitalWrite(rpin, LOW);
+  delay(1000);
+  digitalWrite(gpin, LOW);
+  digitalWrite(ampin, LOW);
+  digitalWrite(rpin, HIGH);
+  delay(1000);
+  
+  
+  
 
 }
