4dc1d5f6853a900275702b693a0cf34d893defdc
Tenyson05 <Rojahog@hotmail.com>
2019-03-19 12:08:35 -0700

    fixed delay

--- TOC_Project.ino
+++ TOC_Project.ino
@@ -26,9 +26,6 @@ void loop() {
   delay(20000);
   
   
-  //so as things are nnow the green light shall turn on for 30 seconds then move on to amber for 5 seconds then red light for 20 seconds, I would like to adjust it
-//  so that when the light is on green and the walk button is press the green light is shorten by 5 seconds if the current time of the green light is less than 20 seconds
-//if the walk button is press while the light is on amber or red the time of the red light shall be extended by 5 second, if it is not press nothing shall happen
-// 
+  
 
 }
