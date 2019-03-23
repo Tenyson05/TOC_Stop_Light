c55c25b5f4902ae32b0a79baf79def588a19f021
Tenyson05 <Rojahog@hotmail.com>
2019-03-23 15:39:56 -0700

    Added button functionality

--- TOC_Project.ino
+++ TOC_Project.ino
@@ -1,30 +1,60 @@
-#include "Timer.h" 
-
+//#include "Timer.h" 
+//may have to change pin nums later
 const int rpin = 3;
 const int ampin = 4;
 const int gpin = 5;
-const int wbutton = 10;
-//may have to change pin nums later
+const int wButton = 10;
+int button;
+
 void setup() {
   // put your setup code here, to run once:
   pinMode(rpin, OUTPUT);
   pinMode(ampin, OUTPUT);
-  pinMode(gpin, OUTPUT);    
+  pinMode(gpin, OUTPUT);   
+  pinMode(wButton, INPUT); 
 }
 
 void loop() {
-  // put your main code here, to run repeatedly:
-  digitalWrite(gpin, HIGH);
-  digitalWrite(ampin, LOW);
-  digitalWrite(rpin, LOW);
-  delay(30000);
-  digitalWrite(gpin, LOW);
-  digitalWrite(ampin, HIGH);
-  digitalWrite(rpin, LOW);
-  delay(5000);
-  digitalWrite(gpin, LOW);
-  digitalWrite(ampin, LOW);
-  digitalWrite(rpin, HIGH);
-  delay(20000); 
+  button = digitalRead(wButton);
+  green();
+  greenLightButton();
+  amber();
+  delay(3000);
+  red();
+  delay(3000);  
+}
 
+void red() {
+  digitalWrite(ampin, LOW);
+  digitalWrite(rpin, HIGH);  
+}
+
+void green() {
+  digitalWrite(rpin, LOW);
+  digitalWrite(gpin, HIGH);   
+}
+
+void amber() {
+  digitalWrite(gpin, LOW);
+  digitalWrite(ampin, HIGH); 
+}
+
+void off() {
+  digitalWrite(gpin, LOW);
+  digitalWrite(ampin, LOW);
+  digitalWrite(rpin, LOW);  
+}
+
+void greenLightButton() {
+  for (int i = 1; i < 10; i++) {
+    int buttonState = digitalRead(wButton);
+
+    if(buttonState == LOW) {
+      delay(1000);
+    }
+    else if(buttonState == HIGH) {
+      delay(5000);
+      return;
+    }
+  }
 }
