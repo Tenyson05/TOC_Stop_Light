232a371ca1459dce0054fa019b210fc155f2e7f0
Tenyson05 <Rojahog@hotmail.com>
2019-04-02 18:35:50 -0700

    Adjusted the timer for each light

--- TOC_Project.ino
+++ TOC_Project.ino
@@ -17,11 +17,11 @@ void setup() {
 void loop() {
   button = digitalRead(wButton);
   green();
-  greenLightButton();
+  delay(6000);
   amber();
-  delay(3000);
+  amberButton();
   red();
-  delay(3000);  
+  redButton();  
 }
 
 void red() {
@@ -39,22 +39,34 @@ void amber() {
   digitalWrite(ampin, HIGH); 
 }
 
-void off() {
-  digitalWrite(gpin, LOW);
-  digitalWrite(ampin, LOW);
-  digitalWrite(rpin, LOW);  
-}
+//The amberButton and red button hanges the delay of the red lights if
+//the buttoms are press if the button is not press the lights go through
+//it's normal light cycle
 
-void greenLightButton() {
-  for (int i = 1; i < 10; i++) {
+void amberButton() {
+  for(int i = 1; i <10; i++) {
     int buttonState = digitalRead(wButton);
 
     if(buttonState == LOW) {
-      delay(1000);
-    }
-    else if(buttonState == HIGH) {
       delay(5000);
-      return;
+    }
+
+    else if (buttonState == HIGH) {
+      delay(2000);
     }
   }
+}
+
+void redButton() {
+  for(int i=1; i < 10; i++){
+    int buttonState = digitalRead(wButton);
+    
+    if(buttonState == LOW) {
+      delay(5000);
+    }
+    else if(buttonState == HIGH) {
+      delay(3000);
+  }
+ }
+    
 }
